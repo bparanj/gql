c4c3d8aafa3a2eb1e09db1fae7da6866ee2da871
Bala Paranj <bparanj@onekingslane.com>
2016-12-15 15:51:10 -0800

    Remove unnecessary code

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -3,8 +3,7 @@ class QueriesController < ApplicationController
     
   def create
     query_string = params[:query]
-    query_variables = params[:variables] || {}
-    result = BlogSchema.execute(query_string, variables: query_variables)
+    result = BlogSchema.execute(query_string)
     render json: result
   end
 end
